ed72563e31cfd962c599a02c24e65551bd005efc
khghouse <khghouse@naver.com>
2024-03-02 21:04:07 +0900

    @Transactional 설정

--- src/main/java/com/board/service/article/ArticleService.java
+++ src/main/java/com/board/service/article/ArticleService.java
@@ -6,11 +6,13 @@ import com.board.service.article.request.ArticleCreateServiceRequest;
 import com.board.service.article.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
@@ -18,13 +20,14 @@ public class ArticleService {
     /**
      * 게시글을 등록한다.
      */
+    @Transactional
     public ArticleResponse postArticle(ArticleCreateServiceRequest request) {
         Article article = articleRepository.save(request.toEntity());
         return ArticleResponse.of(article);
     }
 
     /**
-     * 게시글 1건을 조회한다.s
+     * 게시글 1건을 조회한다.
      */
     public ArticleResponse getArticle(Long id) {
         Article article = articleRepository.findById(id)
