b6faffaf1df980905f93dc26c167901094ff28bb
khghouse <khghouse@naver.com>
2024-04-25 17:01:32 +0900

    JWT Token Provider 테스트 작성

--- src/test/java/com/board/provider/JwtTokenProviderTest.java
+++ src/test/java/com/board/provider/JwtTokenProviderTest.java
@@ -5,7 +5,9 @@ import com.board.domain.member.Member;
 import com.board.domain.member.MemberRepository;
 import com.board.dto.jwt.JwtToken;
 import com.board.dto.security.SecurityUser;
+import com.board.enumeration.JwtErrorCode;
 import com.board.exception.ForbiddenException;
+import com.board.exception.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -18,6 +20,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -78,7 +81,7 @@ class JwtTokenProviderTest extends IntegrationTestSupport {
         // then
         assertThat(result.getName()).isEqualTo("khghouse@daum.net");
         assertThat(user.getEmail()).isEqualTo("khghouse@daum.net");
-        assertThat(user.getMemberId()).isEqualTo(1L);
+        assertThat(user.getMemberId()).isEqualTo(member.getId());
     }
 
     @Test
@@ -104,19 +107,107 @@ class JwtTokenProviderTest extends IntegrationTestSupport {
                 .isInstanceOf(ForbiddenException.class)
                 .hasMessage("권한 정보가 없는 토큰입니다.");
     }
-//
-//    @Test
-//    @DisplayName("생성된 액세스 토큰을 파싱했지만, 유효기간 만료로 예외가 발생한다.")
-//    void getAuthenticationExpiration() throws Exception {
-//        // given
-//        ReflectionTestUtils.setField(jwtTokenProvider, "expirationMillisecord", 1L);
-//        String accessToken = jwtTokenProvider.createAccessToken(1L);
-//        TimeUnit.SECONDS.sleep(1);
-//
-//        // when, then
-//        assertThatThrownBy(() -> jwtTokenProvider.getAuthentication(accessToken))
-//                .isInstanceOf(UnauthorizedException.class)
-//                .hasMessage("인증 정보가 유효하지 않습니다.");
-//    }
+
+    @Test
+    @DisplayName("액세스 토큰을 이용하여 회원 ID를 조회한다.")
+    void getMemberIdByAccessToken() {
+        // given
+        String accessToken = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode("2z6oNf/GwepEYumNk5rJSIyADL+WQ3YrArbVv+LTQJs=")))
+                .claim("memberId", 1L)
+                .expiration(new Date(System.currentTimeMillis() + 3600L * 1000L))
+                .compact();
+
+        // when
+        Long result = jwtTokenProvider.getMemberIdByAccessToken(accessToken);
+
+        // then
+        assertThat(result).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("만료된 액세스 토큰을 이용하여 회원 ID를 조회한다.")
+    void getMemberIdByExpiredAccessToken() throws Exception {
+        // given
+        String accessToken = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode("2z6oNf/GwepEYumNk5rJSIyADL+WQ3YrArbVv+LTQJs=")))
+                .claim("memberId", 1L)
+                .expiration(new Date(System.currentTimeMillis() + 1000L))
+                .compact();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // when
+        Long result = jwtTokenProvider.getMemberIdByAccessToken(accessToken);
+
+        // then
+        assertThat(result).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("액세스 토큰의 유효성을 검증하고 성공한다.")
+    void validateToken() {
+        // given
+        String accessToken = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode("2z6oNf/GwepEYumNk5rJSIyADL+WQ3YrArbVv+LTQJs=")))
+                .expiration(new Date(System.currentTimeMillis() + 3600L * 1000L))
+                .compact();
+
+        // when
+        boolean result = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("액세스 토큰의 유효성 체크에서 손상된 토큰으로 예외가 발생한다.")
+    void validateTokenMalformedJwtException() {
+        // when, then
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken("json.web.token"))
+                .isInstanceOf(JwtException.class)
+                .satisfies(e -> {
+                    JwtException jwtException = (JwtException) e;
+                    assertThat(jwtException.getJwtErrorCode()).isEqualTo(JwtErrorCode.MALFORMED);
+                });
+    }
+
+    @Test
+    @DisplayName("액세스 토큰의 유효성 체크에서 만료된 토큰으로 예외가 발생한다.")
+    void validateTokenExpiredJwtException() throws Exception {
+        // given
+        String accessToken = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode("2z6oNf/GwepEYumNk5rJSIyADL+WQ3YrArbVv+LTQJs=")))
+                .expiration(new Date(System.currentTimeMillis() + 1000L))
+                .compact();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // when, then
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(accessToken))
+                .isInstanceOf(JwtException.class)
+                .satisfies(e -> {
+                    JwtException jwtException = (JwtException) e;
+                    assertThat(jwtException.getJwtErrorCode()).isEqualTo(JwtErrorCode.EXPIRED);
+                });
+    }
+
+    @Test
+    @DisplayName("다른 키 값으로 생성된 액세스 토큰은 유효성 체크에서 예외가 발생한다.")
+    void validateTokenInvalidKey() {
+        // given
+        String accessToken = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode("yXbYyWTRsYQwoe6Tm+z5ujQ/Irmi34d3QEzJQAxuRwQ=")))
+                .expiration(new Date(System.currentTimeMillis() + 3600L * 1000L))
+                .compact();
+
+        // when, then
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(accessToken))
+                .isInstanceOf(JwtException.class)
+                .satisfies(e -> {
+                    JwtException jwtException = (JwtException) e;
+                    assertThat(jwtException.getJwtErrorCode()).isEqualTo(JwtErrorCode.INVALID);
+                });
+    }
 
 }
