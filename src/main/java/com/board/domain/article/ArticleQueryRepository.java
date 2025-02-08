058c8d81d4c6d6e70e125b44f67f84337e467c63
khghouse <khghouse@naver.com>
2025-02-08 23:32:36 +0900

    Querydsl 페이징 처리 테스트

--- src/main/java/com/board/domain/article/ArticleQueryRepository.java
+++ src/main/java/com/board/domain/article/ArticleQueryRepository.java
@@ -1,10 +1,17 @@
 package com.board.domain.article;
 
 import com.board.domain.member.QMember;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.ComparableExpressionBase;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,6 +28,38 @@ public class ArticleQueryRepository {
                 .where(article.deleted.isFalse())
                 .fetchJoin()
                 .fetch();
+    }
+
+    public List<Article> findActiveArticles(Pageable pageable) {
+        List<OrderSpecifier<?>> orderSpecifiers = getOrderSpecifiers(pageable, article);
+
+        return queryFactory.selectFrom(article)
+                .innerJoin(article.member, member)
+                .where(article.deleted.isFalse())
+                .orderBy(orderSpecifiers.get(0))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private List<OrderSpecifier<?>> getOrderSpecifiers(Pageable pageable, QArticle article) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+
+        for (Sort.Order order : pageable.getSort()) {
+            PathBuilder<Article> path = new PathBuilder<>(Article.class, "article");
+            ComparableExpressionBase<?> fieldPath = path.getComparable(order.getProperty(), Comparable.class);
+
+            // 오름차순 정렬
+            if (order.getDirection().isAscending()) {
+                orderSpecifiers.add(new OrderSpecifier<>(Order.ASC, fieldPath));
+            }
+            // 내림차순 정렬
+            else {
+                orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, fieldPath));
+            }
+        }
+
+        return orderSpecifiers;
     }
 
 }
--- src/test/java/com/board/domain/article/ArticleQueryRepositoryTest.java
+++ src/test/java/com/board/domain/article/ArticleQueryRepositoryTest.java
@@ -2,6 +2,7 @@ package com.board.domain.article;
 
 import com.board.domain.member.Member;
 import com.board.domain.member.MemberRepository;
+import com.board.dto.page.PageServiceRequest;
 import com.board.support.RepositoryTestSupport;
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
@@ -59,6 +60,40 @@ class ArticleQueryRepositoryTest extends RepositoryTestSupport {
                         Tuple.tuple("게시글 제목 1", "게시글 내용 1", "khghouse1@daum.net"),
                         Tuple.tuple("게시글 제목 3", "게시글 내용 3", "khghouse3@daum.net"),
                         Tuple.tuple("게시글 제목 4", "게시글 내용 4", "khghouse4@daum.net")
+                );
+    }
+
+    @Test
+    @DisplayName("querydsl 페이징 처리 테스트")
+    void findActiveArticles() {
+        // given
+        List<Member> members = IntStream.range(1, 5)
+                .mapToObj(i -> Member.builder().email("khghouse" + i + "@daum.net").password("Password12#$").build())
+                .collect(Collectors.toList());
+        memberRepository.saveAll(members);
+
+        Article article1 = toEntity("게시글 제목 1", "게시글 내용 1", false, members.get(0));
+        Article article2 = toEntity("게시글 제목 2", "게시글 내용 2", true, members.get(1));
+        Article article3 = toEntity("게시글 제목 3", "게시글 내용 3", false, members.get(2));
+        Article article4 = toEntity("게시글 제목 4", "게시글 내용 4", false, members.get(3));
+        articleRepository.saveAll(List.of(article1, article2, article3, article4));
+
+        testEntityManager.flush();
+        testEntityManager.clear();
+        System.out.println("---------------------------");
+
+        PageServiceRequest request = PageServiceRequest.of(1, 10, "desc", "id");
+
+        // when
+        List<Article> result = articleQueryRepository.findActiveArticles(request.toPageable());
+
+        // then
+        assertThat(result).hasSize(3)
+                .extracting("title", "content", "member.email")
+                .containsExactly(
+                        Tuple.tuple("게시글 제목 4", "게시글 내용 4", "khghouse4@daum.net"),
+                        Tuple.tuple("게시글 제목 3", "게시글 내용 3", "khghouse3@daum.net"),
+                        Tuple.tuple("게시글 제목 1", "게시글 내용 1", "khghouse1@daum.net")
                 );
     }
 
